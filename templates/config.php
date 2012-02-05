f3b01006027d3668e20055f02c42f060aeefb5c3
supervolli <github@volkerkuhle.de>
2012-02-05 11:20:08 +0100

    Zurück-Knopf eingefügt

--- templates/config.php
+++ templates/config.php
@@ -1,4 +1,12 @@
 <?php
-  $mh = "{".$_['mailhost'].":993/imap/ssl/novalidate-cert}";
-  echo $mh;
+$uid = $_['uid'];
+$mh = $_['mailhost'];
+$mu = $_['mailuser'];
+$mp = $_['mailpwd'];
+
 ?>
+
+<div id="controls">
+<input type="button" id="email_Posteingang" value="Posteingang" onClick="window.location.replace('index.php?t=index')"></input>
+</div>
+
