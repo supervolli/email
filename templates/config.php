e61103cdfb699ff61761f07511392a249479c8ee
supervolli <github@volkerkuhle.de>
2012-02-05 14:16:32 +0100

    Konfigurationsformular Teil 1

--- templates/config.php
+++ templates/config.php
@@ -3,7 +3,17 @@ $uid = $_['uid'];
 $mh = $_['mailhost'];
 $mu = $_['mailuser'];
 $mp = $_['mailpwd'];
-
-echo "Emaileinstellungen";
 ?>
+<form id="emailform">
+  <fieldset class="personalblock">
+    <strong>Emaileinstellungen</strong>
+    <label for="mh">IMAP-Server</label>
+    <input type="text" name="mh" id="mh" value="<?php echo $mh; ?>" placeholder="IMAP-Server" original-title="">
+    <br>
+    <label for="mu">Benutzername</label>
+    <input type="text" name="mu" id="mu" value="<?php echo $mu; ?>" placeholder="Benutzername" original-title="">
+    <label for="mp">Passwort</label>
+    <input type="password" name="mp" id="mp" value="" placeholder="Passwort" original-title="">   
+  </fieldset>
+</form>
 
