cdcb2efbd4de6f125eedc0ededd6ad75f7bc8347
supervolli <github@volkerkuhle.de>
2012-02-06 06:46:07 +0100

    Email wird geladen

--- msg.php
+++ msg.php
@@ -1,5 +1,27 @@
 <?php
   require_once('../../lib/base.php');
+  OC_Util::checkLoggedIn();
+  OC_Util::checkAppEnabled('email');
+  
+  # Variablen laden
   $uid = OC_User::getUser();
-  echo $uid;
+  $msg = $_GET['msg'];
+  
+  # Email Verbindung aus DB holen
+  $query = OC_DB::prepare("SELECT * FROM *PREFIX*email_connection WHERE uid='".$uid."'");
+  $data = $query->execute(array('bar'))->fetchAll();
+  $mu = $data[0]['mailuser']; 
+  $mh = $data[0]['mailhost'];
+  $mp = $data[0]['mailpwd'];
+  $mport = $data[0]['mailport'];
+  $mssl = $data[0]['mailssl'];
+  
+  # SSL enabled?
+  $mssl = ( $mssl ) ? '/ssl' : '' ;
+  
+  # Server String zusammensetzen
+  $mh = '{'.$mh.':'.$mport.'imap'.$mssl.'/novalidate-cert}INBOX';
+  
+  $mbox = imap_open($mh."INBOX", $mu, $mp);
+  echo imap_last_error();
 ?>
