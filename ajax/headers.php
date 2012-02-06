5ded1c4e00107fff36f25f38f4a50cc2dba94e3e
supervolli <github@volkerkuhle.de>
2012-02-06 23:11:08 +0100

    Change-Id: I683a389b28eecabec17198fd7c1cc6ca6c51f1b6

--- ajax/headers.php
+++ ajax/headers.php
@@ -39,7 +39,7 @@ for ( $i=$offset; $i < ( $offset + 20 ); $i++ ){
 		$subject = imap_utf8( $header->subject );
 		# Mail von heute?
 		$datetmp = $header->udate;
-		$date = ( date("d M Y") == date("d M Y", $datetmp) ) ? date( "H:m", $datetmp ) : date( "d. M. y, $datetmp" );
+		$date = ( date("d M Y") == date("d M Y", $datetmp) ) ? date( "H:m", $datetmp ) : date( "d. M. y", $datetmp );
  		$from = imap_utf8( $header->fetchfrom );
 		$message_id = $header->message_id;
 		$unseen = $header->Unseen;
