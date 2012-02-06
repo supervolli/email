8874a6a5a41369d33cf5c909c24c0bd4b77ce6b5
supervolli <github@volkerkuhle.de>
2012-02-06 23:35:47 +0100

    Change-Id: If9ebc821397be4a830789980c27666780af606cc

--- ajax/headers.php
+++ ajax/headers.php
@@ -47,11 +47,11 @@ for ( $i=$offset; $i < ( $offset + 20 ); $i++ ){
 		$answered = $header->Answered;
 	#	# Ausgabe eines Headers
 		echo '<li class="header">';
-		echo '<table>'
+		echo '<table>';
 		echo '  <tr><td class="from">'.$from.'</td><td class="date">'.$date.'</td></tr>';
 		echo '  <tr><td class="subject'.$unseen.'" colspan="2">'.$subject.'</td></tr>';
 		echo '  <tr><td class="mailbody" colspan="2">Ein kleines bisschen vom Body...</td></tr>';
-		echo '</table>'
+		echo '</table>';
 		echo '</li>';
     }
 }
