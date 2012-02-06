8f7584bc4e84b672009c4dc10835a1152a8fa4e4
supervolli <github@volkerkuhle.de>
2012-02-06 23:00:19 +0100

    Change-Id: I95406dc83a79ffffdd14c47bc4ba05d365e8e032

--- ajax/headers.php
+++ ajax/headers.php
@@ -35,19 +35,19 @@ echo '<ul class="headers">';
 for ( $i=$offset; $i < ( $offset + 20 ); $i++ ){
 	# Header einer Mail laden
 	$header = imap_headerinfo( $mbox,( $anzahl - $i ), 20, 100 );
-
-	$subject = imap_utf8( $header->subject );
-#	$date = date( "d. M Y H:m",$header->udate );
-#	$from = imap_utf8( $header->fetchfrom );
-#	$message_id = $header->message_id;
-#	$unseen = $header->Unseen;
-#	# Ausgabe eines Headers
-	echo '<li class="header">';
-#	echo '  <b>'.$from.' '.$date'</b><br>';
-	echo '  '.$subject.'<br>';
-	echo '   Ein kleines bisschen vom Body...';
-	echo '</li>';
-
+    if ( $header ) {
+		$subject = imap_utf8( $header->subject );
+	#	$date = date( "d. M Y H:m",$header->udate );
+	#	$from = imap_utf8( $header->fetchfrom );
+	#	$message_id = $header->message_id;
+	#	$unseen = $header->Unseen;
+	#	# Ausgabe eines Headers
+		echo '<li class="header">';
+	#	echo '  <b>'.$from.' '.$date'</b><br>';
+		echo '  '.$subject.'<br>';
+		echo '   Ein kleines bisschen vom Body...';
+		echo '</li>';
+    }
 }
 
 echo '</ul>';
