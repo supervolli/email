f2a804de8f27618d3defaf15fac58b321a82deeb
supervolli <github@volkerkuhle.de>
2012-02-06 22:04:30 +0100

    Change-Id: Id7de422c965972b006e305de2c6fc41f262031a1

--- ajax/headers.php
+++ ajax/headers.php
@@ -34,11 +34,11 @@ $anzahl = imap_num_msg( $mbox );
 
 for ($i=$offset; $i < ($offset + 20); $i++){
 	$header = imap_headerinfo($mbox,($anzahl - $i), 20, 100);
-#	$subject = imap_utf8($header->subject);
-#	$date = date("d. M Y H:m",$header->udate);
-#	$from = imap_utf8($header->fetchfrom);
+	$subject = imap_utf8($header->subject);
+	$date = date("d. M Y H:m",$header->udate);
+	$from = imap_utf8($header->fetchfrom);
 #	$message_id = $header->message_id);
-#	$unseen = $header->Unseen;
+	$unseen = $header->Unseen;
 }
 
 if( $mbox ) {
