6a0b15286219aa846ef7f8ced78b0b6584707c9b
supervolli <github@volkerkuhle.de>
2012-02-06 16:46:29 +0100

    merge 2

--- index.php
+++ index.php
@@ -24,25 +24,6 @@ $mailpwd=$data[0]['mailpwd'];
 $mailport=$data[0]['mailport'];
 $mailssl=$data[0]['mailssl'];
 
-# Welche Seite soll geladen werden?
-#$templates = array("index","config");
-#$t=$_GET['t'];
-#if ( in_array($t, $templates) ) {
-#  $template = $_GET['t'];
-#} else {
-#  $template = "index";
-#}
-
-# Aktive Postfach, wenn nicht gesetzt, dann 0
-#$factive = isset($_GET['factive']) ? $_GET['factive'] : 0;
-
-# Welche Header sollen angezeigt werden (ab welcher Mail)
-#$hstart  = isset($_GET['hstart']) ? $_GET['hstart'] : 0;
-
-# Anzuzeigende Email
-#$msg  = isset($_GET['msg']) ? $_GET['msg'] : 'nomail';
-
-
 $tmpl = new OC_TEMPLATE( "email", "index", "user" );
 $tmpl->assign( "uid",  $uid);
 $tmpl->assign( "mailuser",  $mailuser);
@@ -50,8 +31,5 @@ $tmpl->assign( "mailhost",  $mailhost);
 $tmpl->assign( "mailpwd",  $mailpwd);
 $tmpl->assign( "mailport",  $mailport);
 $tmpl->assign( "mailssl",  $mailssl);
-#$tmpl->assign( "factive",  $factive);
-#$tmpl->assign( "hstart",  $hstart);
-#$tmpl->assign( "msg",  $msg);
 $tmpl->printPage();
 ?>
