56544a606a6c39546961f430bc30893ef271c833
supervolli <github@volkerkuhle.de>
2012-02-06 16:54:58 +0100

    Javascript Funktionen zum Content nachladen

---
+++ js/getContent.js
@@ -0,0 +1,40 @@
+var xmlHttpObject = false;
+
+if (typeof XMLHttpRequest != 'undefined') 
+{
+    xmlHttpObject = new XMLHttpRequest();
+}
+if (!xmlHttpObject) 
+{
+    try 
+    {
+        xmlHttpObject = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch(e) 
+    {
+        try 
+        {
+            xmlHttpObject = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch(e) 
+        {
+            xmlHttpObject = null;
+        }
+    }
+}
+
+function loadContent(site, target)
+{
+    xmlHttpObject.open('get',site);
+    xmlHttpObject.onreadystatechange = handleContent(target);
+    xmlHttpObject.send(null);
+    return false;
+}
+
+function handleContent(target)
+{
+    if (xmlHttpObject.readyState == 4)
+    {
+        document.getElementById(target).innerHTML = xmlHttpObject.responseText;
+    }
+}
