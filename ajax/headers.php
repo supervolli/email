9e178fbe15b84fb0cf4fe8ea8b44de39bcc1810f
supervolli <github@volkerkuhle.de>
2012-02-06 21:44:44 +0100

    Change-Id: If4985129974ef4be6deb3e4d67e6524ac4821878

--- ajax/headers.php
+++ ajax/headers.php
@@ -1,6 +1,6 @@
 <?php
 $folder = $_GET['folder']; # zu oeffnendes Postfach
-$offset = $_GET['offset']; # ab welcher Mail soll geladen werden
+#$offset = $_GET['offset']; # ab welcher Mail soll geladen werden
 
 echo $folder;
 ?>
