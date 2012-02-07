85bba4a6a009114a08b4ab08a1dd463b7aa3d712
supervolli <github@volkerkuhle.de>
2012-02-07 23:37:27 +0100

    Change-Id: I781e200387296e4981ebc5fdc4d2809c62cbd6c6

--- ajax/mail_decode.php
+++ ajax/mail_decode.php
@@ -23,54 +23,13 @@ function getBody( $mbox, $msgno ){
 			$body = convert_html_to_text($body);	
 		}	
 	} else {       # Multipart
-		$body = get_part($mbox, $msgno, "TEXT/PLAIN")
+		$body = $struct->part;
 		#$body = imap_fetchbody( $mbox, $msgno, 1, 2 );
 		#$body = $type.' '.$subtype;
 	}
 	
 	return $body;
 }
-
-function get_mime_type(&$structure) { 
-    $primary_mime_type = array("TEXT", "MULTIPART", "MESSAGE", "APPLICATION", "AUDIO", "IMAGE", "VIDEO", "OTHER"); 
-    if($structure->subtype) { 
-         return $primary_mime_type[(int) $structure->type] . '/' . $structure->subtype; 
-     } 
-     return "TEXT/PLAIN"; 
-} 
-
-function get_part($stream, $msg_number, $mime_type, $structure = false, $part_number = false) { 
-    if (!$structure) { 
-         $structure = imap_fetchstructure($stream, $msg_number); 
-     } 
-    if($structure) { 
-         if($mime_type == get_mime_type($structure)) { 
-              if(!$part_number) { 
-                   $part_number = "1"; 
-               } 
-              $text = imap_fetchbody($stream, $msg_number, $part_number); 
-              if($structure->encoding == 3) { 
-                   return imap_base64($text); 
-               } else if ($structure->encoding == 4) { 
-                   return imap_qprint($text); 
-               } else { 
-                   return $text; 
-            } 
-        } 
-         if ($structure->type == 1) { /* multipart */ 
-              while (list($index, $sub_structure) = each($structure->parts)) { 
-                if ($part_number) { 
-                    $prefix = $part_number . '.'; 
-                } 
-                $data = get_part($stream, $msg_number, $mime_type, $sub_structure, $prefix . ($index + 1)); 
-                if ($data) { 
-                    return $data; 
-                } 
-            } 
-        } 
-    } 
-    return false; 
-} 
 
 
 ?>
