10f7284f517dd7c2525a606ca79814ff1172fa8b
supervolli <github@volkerkuhle.de>
2012-02-07 21:56:36 +0100

    Change-Id: I031eb31c64b31c0b3cabb62fb14b3b53f4982da7

--- ajax/mail_decode.php
+++ ajax/mail_decode.php
@@ -10,13 +10,15 @@ function getBody( $mbox, $msgno ){
 	
 	
 	if ($type==0){ # Text
-		#$body = imap_fetchbody( $mbox, $msgno, 0, 2 );
-		#switch ($encode){
-		#	3: $body = base64_decode($body);
-		#	   break;
-		#	4: $body = quoted_printable_decode($body);
-		#	
-		#}
+		$body = imap_fetchbody( $mbox, $msgno, 0, 2 );
+		switch ($encode){
+			case 3: 
+				$body = base64_decode($body);
+			    break;
+			case 4: 
+				$body = quoted_printable_decode($body);
+			    break;
+		}
 		$body = 'text';		
 	} else {       # Multipart
 		
