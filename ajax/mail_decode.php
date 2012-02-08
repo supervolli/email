e3f700f699b4debb57766cc99323b0587cc565be
supervolli <github@volkerkuhle.de>
2012-02-08 15:37:29 +0100

    Change-Id: I7f00100d3911432b618d10909f705a3025a0c369

--- ajax/mail_decode.php
+++ ajax/mail_decode.php
@@ -3,35 +3,15 @@
 function getBody( $mbox, $msgno ){
 	$body = 'empty';
 	
-	$struct  = imap_fetchstructure( $mbox, $msgno );
-	$encode  = $struct->encoding;
-	$type    = $struct->type;
-	$subtype = ( $struct->ifsubtype ) ? $struct->subtype : 'text';
-	
-	
-	if ($type==0){ # Text
-		$body = imap_fetchbody( $mbox, $msgno, 1, 2 );
-		switch ($encode){
-			case 3: 
-				$body = base64_decode($body);
-			    break;
-			case 4: 
-				$body = quoted_printable_decode($body);
-			    break;
-		}
-		if ( strtolower( $subtype ) == 'html' ){
-			$body = convert_html_to_text($body);	
-		}	
-	} else {       # Multipart
-		$body = get_part($mbox, $msgno, "TEXT/PLAIN");
-		if ( $body == '' ) {
-			$body = get_part($mbox, $msgno, "TEXT/HTML");
-		}
-		$body = convert_html_to_text($body);
+	$body = get_part($mbox, $msgno, "TEXT/PLAIN");
+	if ( $body == '' ) {
+		$body = get_part($mbox, $msgno, "TEXT/HTML");
 	}
+	$body = convert_html_to_text($body);
 	
 	return $body;
 }
+
 
 function get_mime_type($structure) { 
     $primary_mime_type = array("TEXT", "MULTIPART", "MESSAGE", "APPLICATION", "AUDIO", "IMAGE", "VIDEO", "OTHER"); 
